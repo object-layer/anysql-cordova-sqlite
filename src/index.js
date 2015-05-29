71b6ed468f3616a8a0a1200a172e5831c13c4a2d
Manuel Vila <mvila@3base.com>
2015-05-29 13:55:06 +0800

    switch to ES6

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 'use strict';
 
-var _ = require('lodash');
-var co = require('co');
-var KindaObject = require('kinda-object');
+let _ = require('lodash');
+let co = require('co');
+let KindaObject = require('kinda-object');
 
-var KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
-  this.setCreator(function(name, options) {
-    if (!options) options = {};
-    var that = this;
+let KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
+  this.setCreator(function(options = {}) {
+    if (!options.name) throw new Error('Cordova SQLite database name is missing');
+    let that = this;
     document.addEventListener('deviceready', function() {
       that.database = window.sqlitePlugin.openDatabase({
-        name: name,
+        name: options.name,
         location: 2
       });
     }, false);
   });
 
   this.initialize = function(cb) {
-    var that = this;
-    var timestamp = Date.now();
-    var check = function() {
-      if (that.database) { cb(); return; }
+    let that = this;
+    let timestamp = Date.now();
+    let check = function() {
+      if (that.database) {
+        cb();
+        return;
+      }
       if (Date.now() - timestamp > 5000) {
         cb(new Error('initialization of KindaCordovaSQLite failed (Cordova didn\'t start after 5 seconds)'));
         return;
@@ -31,18 +34,21 @@ var KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
   };
 
   this.query = function(sql, values) {
-    var that = this;
+    let that = this;
     values = that.normalizeValues(values);
-    var result = undefined;
+    let result;
     return function(cb) {
       that.initialize(function(err) {
-        if (err) { cb(err); return; }
+        if (err) {
+          cb(err);
+          return;
+        }
         that.database.transaction(function(tr) {
-          tr.executeSql(sql, values, function(tr, res) {
+          tr.executeSql(sql, values, function(innerTr, res) {
             result = that.normalizeResult(res);
           });
-        }, function(err) { // transaction error callback
-          cb(err);
+        }, function(innerErr) { // transaction error callback
+          cb(innerErr);
         }, function() { // transaction success callback
           cb(null, result);
         });
@@ -50,44 +56,47 @@ var KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
     };
   };
 
-  this.transaction = function(fn, options) {
-    var that = this;
+  this.transaction = function(fn) {
+    let that = this;
     return function(cb) {
       that.initialize(function(err) {
-        if (err) { cb(err); return; }
-        var lastErr;
-        var transactionAborted;
+        if (err) {
+          cb(err);
+          return;
+        }
+        let lastErr;
+        let transactionAborted;
         that.database.transaction(function(tr) {
           co(function *() {
             try {
               yield fn({
-                query: function(sql, values) {
+                query(sql, values) {
                   values = that.normalizeValues(values);
-                  return function(innerCB) {
+                  return function(innerCb) {
                     try {
-                      tr.executeSql(sql, values, function(tr, res) {
-                        innerCB(null, that.normalizeResult(res));
-                      }, function(tr, err) {
+                      tr.executeSql(sql, values, function(innerTr, res) {
+                        innerCb(null, that.normalizeResult(res));
+                      }, function(innerTr, innerErr) {
                         transactionAborted = true;
-                        innerCB(err);
+                        innerCb(innerErr);
                         return true;
                       });
-                    } catch (err) {
-                      cb(err);
+                    } catch (innerErr) {
+                      cb(innerErr);
                     }
                   };
                 }
               });
-            } catch (err) {
-              lastErr = err;
+            } catch (innerErr) {
+              lastErr = innerErr;
               if (!transactionAborted) {
                 transactionAborted = true;
                 tr.executeSql('arghhhh'); // force the transaction to fail
               }
             }
           })();
-        }, function(err) { // transaction error callback
-          cb(lastErr || err);
+        }, function(innerErr) { // transaction error callback
+          cb(lastErr || innerErr);
         }, function() { // transaction success callback
           cb(null);
         });
@@ -98,10 +107,8 @@ var KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
   this.normalizeValues = function(values) {
     if (values && values.length) {
       values = _.map(values, function(val) {
-        if (typeof val === 'undefined')
-          val = null
-        else if (Buffer.isBuffer(val))
-          val = "bin!" + val.toString('hex');
+        if (typeof val === 'undefined') val = null;
+        else if (Buffer.isBuffer(val)) val = 'bin!' + val.toString('hex');
         return val;
       });
     }
@@ -110,20 +117,25 @@ var KindaCordovaSQLite = KindaObject.extend('KindaCordovaSQLite', function() {
 
   this.normalizeResult = function(result) {
     if (!result) return result;
-    var normalizedResult = [];
-    if (result.rowsAffected != null)
+    let normalizedResult = [];
+    if (result.rowsAffected != null) {
       normalizedResult.affectedRows = result.rowsAffected;
+    }
     try {
-      if (result.insertId != null)
+      if (result.insertId != null) {
         normalizedResult.insertId = result.insertId;
-    } catch (err) {}
+      }
+    } catch (err) {
+      // noop
+    }
     if (!result.rows) return normalizedResult;
-    for (var i = 0; i < result.rows.length; i++) {
-      var row = result.rows.item(i);
-      var normalizedRow = {};
-      _.forOwn(row, function(val, key) {
-        if (val && val.substr && val.substr(0, 4) === 'bin!')
+    for (let i = 0; i < result.rows.length; i++) {
+      let row = result.rows.item(i);
+      let normalizedRow = {};
+      _.forOwn(row, function(val, key) { // eslint-disable-line no-loop-func
+        if (val && val.substr && val.substr(0, 4) === 'bin!') {
           val = new Buffer(val.substr(4), 'hex');
+        }
         normalizedRow[key] = val;
       });
       normalizedResult.push(normalizedRow);
